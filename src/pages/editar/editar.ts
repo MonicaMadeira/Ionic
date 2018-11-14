86671e53d5956a19d2b1e0ae5405c70a59e35df0
Back-End CTC <ctcbackend@outlook.com>
2018-11-14 15:01:30 -0200

    [Mod] Editar

--- src/pages/editar/editar.ts
+++ src/pages/editar/editar.ts
@@ -2,6 +2,7 @@ import { App } from 'ionic-angular';
 import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Provider } from '../../providers/provider/provider';
 
 @Component({
   selector: 'page-editar',
@@ -9,9 +10,19 @@ import { NavController } from 'ionic-angular';
 })
 export class EditarPage {
 
-  constructor(public navCtrl: NavController) {
+  posts: any;
 
-  }
-
+  constructor(public navCtrl: NavController,public Provider: Provider) {
+    
+      this.getPosts();
+    }
  
+    getPosts(){
+      this.Provider.getPostagem()
+      .then(data=>{
+        this.posts = data;
+        console.log(this.posts);
+      });
+    }
+
 }
